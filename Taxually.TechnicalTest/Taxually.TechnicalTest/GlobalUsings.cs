62cd83c5052480cbaabfe15a27ce144f4e7d5db2
mark.kelemen <mark.kelemen@pwc.com>
2024-06-19 01:12:02 +0200

    main: globalusings

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using System.Text;
+global using System.Xml.Serialization;
+global using Taxually.TechnicalTest;
+global using Taxually.TechnicalTest.Interfaces;
+global using Taxually.TechnicalTest.Services;
