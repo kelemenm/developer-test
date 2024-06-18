086c4cd0ec172d1fec45318f613677da49490f85
mark.kelemen <mark.kelemen@pwc.com>
2024-06-19 01:02:12 +0200

    main: DI registration

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
@@ -2,6 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<ICompanyApiRegistrationService, CompanyApiRegistrationService>();
+builder.Services.AddScoped<ICompanyCsvRegistrationService, CompanyCsvRegistrationService>();
+builder.Services.AddScoped<ICompanyVatRegistrationService, CompanyVatRegistrationService>();
+builder.Services.AddScoped<ICompanyXmlRegistrationService, CompanyXmlRegistrationService>();
+builder.Services.AddScoped<ITaxuallyQueueClient, TaxuallyQueueClient>();
+builder.Services.AddScoped<ITaxuallyHttpClient, TaxuallyHttpClient>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
