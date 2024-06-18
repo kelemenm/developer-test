12ac951343c21610213ce9254bcd692a5bb309f8
mark.kelemen <mark.kelemen@pwc.com>
2024-06-19 01:15:19 +0200

    main: get UkUrl from config

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Services/CompanyApiRegistrationService.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/CompanyApiRegistrationService.cs
@@ -2,18 +2,22 @@
 {
     public class CompanyApiRegistrationService : ICompanyApiRegistrationService
     {
-        private const string UkUrl = "https://api.uktax.gov.uk";
+        private readonly string UkUrl;
 
         private readonly ITaxuallyHttpClient taxuallyHttpClient;
 
-        public CompanyApiRegistrationService(ITaxuallyHttpClient taxuallyHttpClient)
+        public CompanyApiRegistrationService(
+            IConfiguration configuration,
+            ITaxuallyHttpClient taxuallyHttpClient)
         {
             this.taxuallyHttpClient = taxuallyHttpClient;
+
+            this.UkUrl = configuration.GetValue<string>("UkUrl");
         }
 
         public async Task RegisterCompany(VatRegistrationRequest request)
         {
-            await this.taxuallyHttpClient.PostAsync(UkUrl, request);
+            await this.taxuallyHttpClient.PostAsync(this.UkUrl, request);
         }
     }
 }
