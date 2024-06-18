31778ae39db498d64992cc4fe0b2178c0b7d1e7c
mark.kelemen <mark.kelemen@pwc.com>
2024-06-19 00:57:50 +0200

    main: RegisterCompanyVatNumber logic added

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Services/CompanyVatRegistrationService.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/CompanyVatRegistrationService.cs
@@ -2,9 +2,38 @@
 {
     public class CompanyVatRegistrationService : ICompanyVatRegistrationService
     {
-        public Task RegisterCompanyVatNumber(VatRegistrationRequest request)
+        private readonly ICompanyApiRegistrationService apiRegistrationService;
+        private readonly ICompanyCsvRegistrationService csvRegistration;
+        private readonly ICompanyXmlRegistrationService xmlRegistrationService;
+
+        public CompanyVatRegistrationService(
+            ICompanyApiRegistrationService apiRegistrationService, 
+            ICompanyCsvRegistrationService csvRegistration, 
+            ICompanyXmlRegistrationService xmlRegistrationService)
         {
-            throw new NotImplementedException();
+            this.apiRegistrationService = apiRegistrationService;
+            this.csvRegistration = csvRegistration;
+            this.xmlRegistrationService = xmlRegistrationService;
+        }
+
+        public async Task RegisterCompanyVatNumber(VatRegistrationRequest request)
+        {
+            switch (request.Country)
+            {
+                case "GB":
+                    // UK has an API to register for a VAT number
+                    await this.apiRegistrationService.RegisterCompany(request);
+                    break;
+                case "FR":
+                    await this.csvRegistration.RegisterCompany(request);
+                    break;
+                case "DE":
+                    await this.xmlRegistrationService.RegisterCompany(request);
+                    break;
+                default:
+                    throw new Exception("Country not supported");
+
+            }
         }
     }
 }
