e328010e19a4b2a26122278ae0b2f34a628044f1
mark.kelemen <mark.kelemen@pwc.com>
2024-06-27 00:37:04 +0200

    main(refact, nuget): default test class deleted, required nugets installed, global usings

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.UnitTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using AutoFixture;
+global using FluentAssertions;
+global using Moq;
+global using Taxually.TechnicalTest.Interfaces;
+global using Taxually.TechnicalTest.Models;
+global using Taxually.TechnicalTest.Services;
