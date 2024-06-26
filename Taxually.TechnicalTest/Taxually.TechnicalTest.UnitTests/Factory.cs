44d6e60bd65e3910ae763dca1db8c9376543d38a
mark.kelemen <mark.kelemen@pwc.com>
2024-06-27 01:09:16 +0200

    main(tests): factorym globalusings

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.UnitTests/Factory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxually.TechnicalTest.UnitTests;
+
+internal static class Factory
+{
+    internal static VatRegistrationRequest CreateVatRegistrationRequest(string country = "")
+    {
+        return new Fixture()
+            .Build<VatRegistrationRequest>()
+            .WithAutoProperties()
+            .With(x => x.Country, country)
+            .Create();
+    }
+}
--- Taxually.TechnicalTest/Taxually.TechnicalTest.UnitTests/GlobalUsings.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest.UnitTests/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using AutoFixture;
 global using FluentAssertions;
+global using Microsoft.Extensions.Configuration;
 global using Moq;
 global using Taxually.TechnicalTest.Interfaces;
 global using Taxually.TechnicalTest.Models;
